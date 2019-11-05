b9b770d98b886dc6c808cfda6437d455d3d67294
Andreas Martin <andreas.martin@andreasmartin.ch>
2019-11-05 20:01:14 +0100

    Revert

---
+++ digipr-acrm-web/src/test/java/rocks/process/acrm/DigiprAcrmWebApplicationTests.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2019. University of Applied Sciences and Arts Northwestern Switzerland FHNW.
+ * All rights reserved.
+ */
+
+package rocks.process.acrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigiprAcrmWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
